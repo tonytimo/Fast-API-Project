e1557ee22a611c67fef932862b19b5f28d0d6432
Tony <tonytimoshe@gmail.com>
2024-10-21 19:18:55 +0300

    added new routes

--- src/router.py
+++ src/router.py
@@ -3,31 +3,35 @@ import json
 from fastapi import APIRouter
 
 
-def _setup() -> list[dict]:
-    with open("data.json", "r") as f:
+def _setup() -> dict[str, list[dict[str,str|int]]]:
+    with open(file="data.json", mode="r", encoding="utf-8") as f:
         json_str = f.read()
-    data = json.loads(json_str).get("users")
-    return data
+    user_list = json.loads(json_str)
+    return user_list
 
 router = APIRouter()
 data = _setup()
 
 
 @router.get("/users")
-def get_users() -> list[dict[str, str]]:
-    return data
+def get_users() -> list[dict[str, str|int]]:
+    return data.get("users") # type: ignore
 
-@router.get("/users/{id}")
-def get_user(id: str) -> dict[str, str]:
-    for user in data:
-        if id == user.get("id"):
+@router.get("/users/{user_id}")
+def get_user(user_id: int) -> dict[str, str|int]:
+    for user in data.get("users"): # type: ignore
+        if user_id == user.get("user_id"):
             return user
-    
     return {"message": f"there is no such user with id:{id}"}
 
-    # @self.router.post("/user")
-    # def add_user() -> dict[str, str]:
-    #     return {"message": "Hello World from a class-based route"}
+@router.post("/user")
+def add_user(name: str) -> dict[str, str|int]:
+    user_id = data.get("users")[-1].get("user_id")+1 # type: ignore
+    data["users"].append({"user_id": user_id, "name": name})
+    with open(file="data.json", mode="w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+    
+    return {"user_id": user_id, "name": name}
 
 
 def get_router() -> APIRouter:
